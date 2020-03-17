535cbe2e2bd816353f6a595a616e0e4a1dfc98e4
Perera <per24h@csiro.au>
2020-03-17 16:18:48 +1100

    Halfway through question 2

--- bom_data_cleaning.R
+++ bom_data_cleaning.R
@@ -12,3 +12,14 @@ temp_seperated_filtered %>%
   group_by(Station_number)%>%
   summarise(num_rows = n ())
 
+# question 2
+
+temp_seperated_filtered_numeric <- temp_seperated_filtered %>%   
+mutate(t_min = as.numeric(t_min))%>%  
+mutate(t_max = as.numeric(t_max))
+temp_difference <- mutate(temp_seperated_filtered_numeric, temp_difference = t_max-t_min)
+view (temp_difference)
+temp_difference_by_month <- group_by(temp_difference, Month)
+view(temp_difference_by_month)
+
+summary <- summarise (temp_difference_by_month, mean_temp_diff = mean(temp_difference))
